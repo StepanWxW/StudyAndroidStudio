4948709e546f0248bd069ff7d71816131df4da6a
WxW <polyakovs@list.ru>
2022-10-17 19:18:46 +0300

    study. First program

--- app/src/main/java/com/example/studyandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/studyandroidstudio/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.studyandroidstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
